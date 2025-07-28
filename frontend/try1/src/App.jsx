20aab19d0473b76a01e258fe377b9f79dd77137c
2300030824 <2300030824@kluniversity.in>
2025-07-28 10:14:57 +0530

    Initial commit fro frontend

---
+++ frontend/try1/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import p1 from './assets/react.svg'
+import p2 from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={p2} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={p1} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
